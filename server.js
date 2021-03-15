4b0bc327b64c2cc22be9f25b86901823b17ea192
Sonam <son1@publicisgroupe.net>
2021-03-15 14:10:32 +0530

    server js change

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
+// Install express server
 const express = require('express');
+const path = require('path');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
-
-
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
